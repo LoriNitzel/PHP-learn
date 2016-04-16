cf21619eb14ed53974298157615f15240c3b3f74
Lori Nitzel <lorinitzel@mac.com>
2016-04-16 16:50:58 -0600

    practice

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title></title>
+</head>
+<body>
+  <section class="main">
+    <p>PHP</p>
+    <p><?php echo "Hello, World!" ?></p>
+  </section>
+</body>
+</html>
